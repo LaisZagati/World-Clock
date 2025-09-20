ed2b4ad0005eec63dbaeb4aeed8780d4fafe286f
LaisZagati <laisviana97@gmail.com>
2025-09-20 10:06:24 -0400

    added multiple locations to countries selection arrow

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ function updateTime() {
   if (miamiElement) {
     let miamiDateElement = miamiElement.querySelector(".date");
     let miamiTimeElement = miamiElement.querySelector(".time");
-    let miamiTime = moment().tz("America/Miami");
+    let miamiTime = moment().tz("America/New_York");
 
     miamiDateElement.innerHTML = miamiTime.format("MMMM	Do YYYY");
     miamiTimeElement.innerHTML = miamiTime.format(
@@ -37,5 +37,25 @@ function updateTime() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityTimeZone}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+           <div class="time">${cityTime.format(
+             "h:mm:ss"
+           )} <small>${cityTime.format("A")}</small></div>
+  </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
